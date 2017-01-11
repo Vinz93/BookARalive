3c17401470a6ff296d77cdaf0a226aa135022de4
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-01-10 20:51:00 -0400

    change base route on the server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('./build'));
 
-app.get('/*', function (req, res) {
+app.get('/webadmin/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
