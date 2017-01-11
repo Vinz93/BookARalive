4d4d1f49fbad88b3e3f2cd8df9bb51aa004e3408
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-01-11 06:52:21 -0400

    server path

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(9000, () => console.log('server runinng..'));
+
+//35.167.51.31
