aba189954f5d463201997b0ab7db751e0b6aaca9
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-01-16 21:08:24 -0400

    add fake endpoint for login

---
+++ src/api/session.js
@@ -0,0 +1,23 @@
+const fakeDB = {
+  admin: {
+    username: 'admin',
+    pwd: 'admin',
+  }
+};
+
+const delay = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+export const login = (username, password) =>
+  delay(500).then(()=> {
+      if(username === fakeDB.admin.username && password === fakeDB.admin.pwd){
+        const user = {
+          id: 122,
+          username,
+          type: 'admin',
+          jwt: '1lkjslaijsdoai.kahksa.aslhasdku'
+        };
+        return user;
+      }
+        throw Error('something went wrong!');
+  });
