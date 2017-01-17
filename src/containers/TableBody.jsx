2bed2a816e0069a5c319b6fa580fa2e62bf3ff70
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-01-16 21:15:28 -0400

    redirect no loged users

--- src/containers/TableBody.jsx
+++ src/containers/TableBody.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import TableContent from '../components/TableContent';
 import { getFilteredBundles } from '../reducers';
 import * as actions from '../actions';
 
 class TableBody extends React.Component{
   componentDidMount() {
+    const token = localStorage.getItem('token');
+    if(!token){
+      browserHistory.push('/');
+    }
     this.fetchData();
   }
 
