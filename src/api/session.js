cb830b39da9337ccd57dad305c8b2259f4e6212c
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-01-20 10:08:11 -0400

    add /session api call

--- src/api/session.js
+++ src/api/session.js
@@ -1,6 +1,6 @@
 const fakeDB = {
   admin: {
-    username: 'admin',
+    email: 'admin@admin.com',
     pwd: 'admin',
   }
 };
@@ -8,16 +8,33 @@ const fakeDB = {
 const delay = (ms) =>
   new Promise(resolve => setTimeout(resolve, ms));
 
-export const login = (username, password) =>
+export const login2 = (email, password) =>
   delay(500).then(()=> {
-      if(username === fakeDB.admin.username && password === fakeDB.admin.pwd){
+      if(email === fakeDB.admin.email && password === fakeDB.admin.pwd){
         const user = {
           id: 122,
-          username,
-          type: 'admin',
-          jwt: '1lkjslaijsdoai.kahksa.aslhasdku'
+          email,
+          role: 'admin',
+          token: '1lkjslaijsdoai.kahksa.aslhasdku'
         };
         return user;
       }
         throw Error('user doesnt exist!');
   });
+
+const URL = "http://35.167.51.31/api/booksaralive/session"
+
+export const login = (email, password) => {
+  const params = {email, password};
+  const encodedParams = Object.keys(params).map((key) => {
+    return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+  }).join('&');
+
+  return fetch(URL, {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body:encodedParams
+    }).then(res => res.json());
+}
