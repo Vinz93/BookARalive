a0a94288a0aa7210bc74bdf695a400af1d023f04
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-01-26 22:25:39 -0400

    add LicensesTBody component that cares in render licenses

---
+++ src/components/Licenses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LicensesTable from '../containers/LicensesTable';
+import Header from './Header';
+
+const Licenses = () => (
+  <div>
+    <Header />
+    <LicensesTable />
+  </div>
+);
+
+export default Licenses;
---
+++ src/components/LicensesTBody.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const valuesOrder = bundle => {
+  const values = [];
+  values.push(bundle['holder']);
+  values.push(bundle['total_slots']);
+  values.push(bundle['exp_date']);
+  values.push(bundle['book_code']);
+  values.push(bundle['edu_contact']);
+  values.push(bundle['adm_contact']);
+  return values;
+}
+
+const LicensesTBody = ({ licenses }) =>
+ (
+   <tbody>
+     {licenses.map(b => (
+       <TableRow
+         key={b.id}
+         license={valuesOrder(b)}
+       />
+    ))}
+   </tbody>
+);
+
+
+LicensesTBody.propTypes = {
+  licenses: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+};
+
+const TableRow = ({ license }) => (
+  <tr>
+    {license.map((info, i) => <td key={i}>{info}</td>)}
+  </tr>
+);
+
+TableRow.propTypes = {
+  license: React.PropTypes.array.isRequired,
+};
+
+
+export default LicensesTBody;
