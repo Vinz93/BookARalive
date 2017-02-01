45f5149933b5e0679ddb006e4c29df93ff05ff40
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-02-01 15:30:32 -0400

    change seachfilter reducer, clean on fecth

--- src/reducers/searchFilter.js
+++ src/reducers/searchFilter.js
@@ -3,6 +3,8 @@ const searchFilter = (state = '', action) => {
     case 'SET_SEARCH':
       return action.searchFilter;
     case 'USER_LOGOUT':
+    case 'FETCH_LICENSES_REQUEST':
+    case 'FETCH_BUNDLES_REQUEST':
       return '';
     default:
       return state;
